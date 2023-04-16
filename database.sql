cfd4c0d239bfbb6f58640a8ecf9fe18c8f6f55fd
Vang Vang <vang2code@gmail.com>
2023-04-15 22:42:32 -0500

    installed pg and edited some comments

--- database.sql
+++ database.sql
@@ -1,15 +1,19 @@
 -- CREATE THE TABLE: 
-CREATE TABLE koalas (
+CREATE TABLE toDo (
 	"id" SERIAL PRIMARY KEY,
 	"toDo" VARCHAR(100),
 	"completed" BOOL,
 );
 
--- Insert multiple rows into the koalas table:
-INSERT INTO toDo
+-- Insert multiple rows into the toDo table:
+IINSERT INTO toDo
 	("toDo", "completed")
 	VALUES
+	('clean dishes', false),
+	('wash car', false),
+	('make coffee', false),
+	('do laundry', false),
+	('go to work', false);
 	
-
 -- In postico, this command will show what is in your table currently:
 SELECT * FROM toDo;
